4c35d70c1e72073e4337dc4d81e690699e56879a
freehere107 <freehere107@gmail.com>
2021-11-23 14:04:15 +0800

    Remove  unnecessary log

--- middleware_logging.go
+++ middleware_logging.go
@@ -10,11 +10,7 @@ type MiddlewareLogging struct{}
 
 func (l *MiddlewareLogging) Call(queue string, message *Msg, next func() bool) (acknowledge bool) {
 	prefix := fmt.Sprint(queue, " JID-", message.Jid())
-
 	start := time.Now()
-	Logger.Println(prefix, "start")
-	Logger.Println(prefix, "args:", message.Args().ToJson())
-
 	defer func() {
 		if e := recover(); e != nil {
 			Logger.Println(prefix, "fail:", time.Since(start))
@@ -28,8 +24,6 @@ func (l *MiddlewareLogging) Call(queue string, message *Msg, next func() bool) (
 	}()
 
 	acknowledge = next()
-
-	Logger.Println(prefix, "done:", time.Since(start))
-
+	Logger.Println(prefix, "args:", message.Args().ToJson(), "done:", time.Since(start))
 	return
 }
